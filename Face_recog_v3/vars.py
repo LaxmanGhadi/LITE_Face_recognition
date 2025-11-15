0b767617d0ca419b0d659351d90429ba0d566222
Laxman V. Ghadigaonkar <88434174+LaxmanGhadi@users.noreply.github.com>
2025-11-15 23:49:01 +0530

    variables file locations

---
+++ Face_recog_v3/vars.py
@@ -0,0 +1,9 @@
+# for size 512 embedding
+Embeddings = 'Face_embedding_512.npz'
+Embedder = r'/home/omotecrpi/Downloads/LITE_Face_recognition-main/facenet_512.tflite'
+Embed_size = 512
+
+# for size 128 embeddding
+# Embeddings = 'Face_embedding_128.npz'
+# Embedder = r'/home/omotecrpi/Downloads/LITE_Face_recognition-main/facenet_128.tflite'
+# Embed_size = 128
